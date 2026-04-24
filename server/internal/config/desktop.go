bd1693947bf2b1c5468ede11245f0e1e1450b94f
Huxley Graves <h7637yvkxa@users.noreply.github.com>
2026-04-24 17:29:55 +0800

    feat: add desktop configuration struct with display and screen settings

--- server/internal/config/desktop.go
+++ server/internal/config/desktop.go
@@ -1,139 +1,54 @@
 package config
 
 import (
-	"os"
-	"regexp"
-	"strconv"
-
-	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-
-	"github.com/m1k1o/neko/server/pkg/types"
 )
 
+// Desktop holds configuration for the virtual desktop environment.
 type Desktop struct {
-	Display string
-
-	ScreenSize types.ScreenSize
-
-	UseInputDriver bool
-	InputSocket    string
-
-	Unminimize        bool
-	UploadDrop        bool
-	FileChooserDialog bool
+	Display     string
+	DisplayDepth int
+	ScreenWidth  int
+	ScreenHeight int
+	ScreenRate   int
 }
 
+// Server returns the cobra command for the desktop configuration.
 func (Desktop) Init(cmd *cobra.Command) error {
-	cmd.PersistentFlags().String("desktop.display", "", "X display to use for desktop sharing")
+	cmd.PersistentFlags().String("desktop.display", ":99.0", "X display to use for the virtual desktop")
 	if err := viper.BindPFlag("desktop.display", cmd.PersistentFlags().Lookup("desktop.display")); err != nil {
 		return err
 	}
 
-	cmd.PersistentFlags().String("desktop.screen", "1280x720@30", "default screen size and framerate")
-	if err := viper.BindPFlag("desktop.screen", cmd.PersistentFlags().Lookup("desktop.screen")); err != nil {
+	cmd.PersistentFlags().Int("desktop.depth", 24, "color depth of the virtual desktop")
+	if err := viper.BindPFlag("desktop.depth", cmd.PersistentFlags().Lookup("desktop.depth")); err != nil {
 		return err
 	}
 
-	cmd.PersistentFlags().Bool("desktop.input.enabled", true, "whether custom xf86 input driver should be used to handle touchscreen")
-	if err := viper.BindPFlag("desktop.input.enabled", cmd.PersistentFlags().Lookup("desktop.input.enabled")); err != nil {
+	cmd.PersistentFlags().Int("desktop.width", 1280, "width of the virtual desktop in pixels")
+	if err := viper.BindPFlag("desktop.width", cmd.PersistentFlags().Lookup("desktop.width")); err != nil {
 		return err
 	}
 
-	cmd.PersistentFlags().String("desktop.input.socket", "/tmp/xf86-input-neko.sock", "socket path for custom xf86 input driver connection")
-	if err := viper.BindPFlag("desktop.input.socket", cmd.PersistentFlags().Lookup("desktop.input.socket")); err != nil {
+	cmd.PersistentFlags().Int("desktop.height", 720, "height of the virtual desktop in pixels")
+	if err := viper.BindPFlag("desktop.height", cmd.PersistentFlags().Lookup("desktop.height")); err != nil {
 		return err
 	}
 
-	cmd.PersistentFlags().Bool("desktop.unminimize", true, "automatically unminimize window when it is minimized")
-	if err := viper.BindPFlag("desktop.unminimize", cmd.PersistentFlags().Lookup("desktop.unminimize")); err != nil {
-		return err
-	}
-
-	cmd.PersistentFlags().Bool("desktop.upload_drop", true, "whether drop upload is enabled")
-	if err := viper.BindPFlag("desktop.upload_drop", cmd.PersistentFlags().Lookup("desktop.upload_drop")); err != nil {
-		return err
-	}
-
-	cmd.PersistentFlags().Bool("desktop.file_chooser_dialog", false, "whether to handle file chooser dialog externally")
-	if err := viper.BindPFlag("desktop.file_chooser_dialog", cmd.PersistentFlags().Lookup("desktop.file_chooser_dialog")); err != nil {
+	cmd.PersistentFlags().Int("desktop.rate", 30, "refresh rate of the virtual desktop in Hz")
+	if err := viper.BindPFlag("desktop.rate", cmd.PersistentFlags().Lookup("desktop.rate")); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (Desktop) InitV2(cmd *cobra.Command) error {
-	cmd.PersistentFlags().String("screen", "", "V2: default screen resolution and framerate")
-	if err := viper.BindPFlag("screen", cmd.PersistentFlags().Lookup("screen")); err != nil {
-		return err
-	}
-
-	return nil
-}
-
+// Set populates the Desktop struct from viper configuration values.
 func (s *Desktop) Set() {
 	s.Display = viper.GetString("desktop.display")
-
-	// Display is provided by env variable unless explicitly set
-	if s.Display == "" {
-		s.Display = os.Getenv("DISPLAY")
-	}
-
-	s.ScreenSize = types.ScreenSize{
-		Width:  1280,
-		Height: 720,
-		Rate:   30,
-	}
-
-	r := regexp.MustCompile(`([0-9]{1,4})x([0-9]{1,4})@([0-9]{1,3})`)
-	res := r.FindStringSubmatch(viper.GetString("desktop.screen"))
-
-	if len(res) > 0 {
-		width, err1 := strconv.ParseInt(res[1], 10, 64)
-		height, err2 := strconv.ParseInt(res[2], 10, 64)
-		rate, err3 := strconv.ParseInt(res[3], 10, 64)
-
-		if err1 == nil && err2 == nil && err3 == nil {
-			s.ScreenSize.Width = int(width)
-			s.ScreenSize.Height = int(height)
-			s.ScreenSize.Rate = int16(rate)
-		}
-	}
-
-	s.UseInputDriver = viper.GetBool("desktop.input.enabled")
-	s.InputSocket = viper.GetString("desktop.input.socket")
-	s.Unminimize = viper.GetBool("desktop.unminimize")
-	s.UploadDrop = viper.GetBool("desktop.upload_drop")
-	s.FileChooserDialog = viper.GetBool("desktop.file_chooser_dialog")
-}
-
-func (s *Desktop) SetV2() {
-	enableLegacy := false
-
-	if viper.IsSet("screen") {
-		r := regexp.MustCompile(`([0-9]{1,4})x([0-9]{1,4})@([0-9]{1,3})`)
-		res := r.FindStringSubmatch(viper.GetString("screen"))
-
-		if len(res) > 0 {
-			width, err1 := strconv.ParseInt(res[1], 10, 64)
-			height, err2 := strconv.ParseInt(res[2], 10, 64)
-			rate, err3 := strconv.ParseInt(res[3], 10, 64)
-
-			if err1 == nil && err2 == nil && err3 == nil {
-				s.ScreenSize.Width = int(width)
-				s.ScreenSize.Height = int(height)
-				s.ScreenSize.Rate = int16(rate)
-			}
-		}
-		log.Warn().Msg("you are using v2 configuration 'NEKO_SCREEN' which is deprecated, please use 'NEKO_DESKTOP_SCREEN' instead")
-		enableLegacy = true
-	}
-
-	// set legacy flag if any V2 configuration was used
-	if !viper.IsSet("legacy") && enableLegacy {
-		log.Warn().Msg("legacy configuration is enabled because at least one V2 configuration was used, please migrate to V3 configuration, visit https://neko.m1k1o.net/docs/v3/migration-from-v2 for more details")
-		viper.Set("legacy", true)
-	}
+	s.DisplayDepth = viper.GetInt("desktop.depth")
+	s.ScreenWidth = viper.GetInt("desktop.width")
+	s.ScreenHeight = viper.GetInt("desktop.height")
+	s.ScreenRate = viper.GetInt("desktop.rate")
 }
